bca21c4e6ae50e4783cccfb36bbca318d1f982c0
luiscazzullo <47572807+luiscazzullo@users.noreply.github.com>
2022-04-29 10:57:11 -0300

    Update Card.js

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,20 +1,13 @@
 import React from 'react';
 
-const Card = ({ appointment, removeAppointment }) => {
-    const { pet, owner, date, hour, symptoms, id} = appointment;
+const Card = ({ appointment }) => {
+    const { patient, time, date, symptoms } = appointment;
     return ( 
         <div className="cita">
-            <p>Mascota: <span>{pet}</span> </p>
-            <p>Dueño: <span>{owner}</span> </p>
+            <p>Mascota: <span>{patient}</span> </p>
             <p>Fecha: <span>{date}</span> </p>
-            <p>Hora: <span>{hour}</span> </p>
+            <p>Hora: <span>{time}</span> </p>
             <p>Síntomas: <span>{symptoms}</span> </p>
-            <button
-            className="u-full-width button eliminar"
-            onClick={() => removeAppointment(id)}
-            >
-            Eliminar &times;
-            </button>
         </div>
      );
 }
